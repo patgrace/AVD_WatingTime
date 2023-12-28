b182c71db00e2ed14a9bd6c43ef078f3fa4c09f9
aaivel <aiveleve@gmail.com>
2023-12-28 09:07:11 +0700

    klasifikasi dan app py

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ y = dataset['Range_Waktu_tunggu']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+# Penyimpanan model
 dt_model = DecisionTreeClassifier(random_state=0)
 dt_model.fit(X_train, y_train)
 
